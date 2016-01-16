755b448752d4f7682927e65939e3cb401a7276d9
mvasile2 <mvasile2@uwo.ca>
2016-01-16 13:20:20 -0500

    Create motionjpeg.js

---
+++ build/motionjpeg.js
@@ -0,0 +1,17 @@
+function motionjpeg(id) {
+    var image = $(id), src;
+
+    if (!image.length) return;
+
+    src = image.attr("src");
+    if (src.indexOf("?") < 0) {
+        image.attr("src", src + "?"); // must have querystring
+    }
+
+    image.on("load", function() {
+        // this cause the load event to be called "recursively"
+        this.src = this.src.replace(/\?[^\n]*$/, "?") +
+            (new Date()).getTime(); // 'this' refers to the image
+    });
+    return image;
+}
